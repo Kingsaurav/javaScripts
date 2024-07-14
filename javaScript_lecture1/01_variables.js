7affdadcaac0e79a60444b635c9928f26deedf2c
saurav kushwaha <147703919+Kingsaurav@users.noreply.github.com>
2024-07-14 14:37:31 +0000

    javaScript_variables

--- javaScript_lecture1/01_variables.js
+++ javaScript_lecture1/01_variables.js
@@ -1,10 +1,16 @@
 const accountId=1234;
 let accountEmail="saurav12@gmail.com";
 var  accountPassword="124444";
+let accountState;// undefined
 accountCity="Jaipur";
 // trying to assign new values in variable
 //accountId=22222;// not allow
 accountEmail="ram@gmail.com";
 accountPassword="232323";
 accountCity="Delhi";
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+/*
+prefer not to use var
+Because of issue in block scope {} and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
