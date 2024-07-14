6176a828c09c2278ae0b70394ec6835dcf373e3a
saurav kushwaha <147703919+Kingsaurav@users.noreply.github.com>
2024-07-14 14:26:12 +0000

    chnaged the front size and editor size

---
+++ javaScript_lecture1/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=1234;
+let accountEmail="saurav12@gmail.com";
+var  accountPassword="124444";
+accountCity="Jaipur";
+// trying to assign new values in variable
+//accountId=22222;// not allow
+accountEmail="ram@gmail.com";
+accountPassword="232323";
+accountCity="Delhi";
+console.table([accountId,accountEmail,accountPassword,accountCity]);
