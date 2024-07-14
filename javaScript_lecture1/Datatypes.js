901a3821c7239b488c3645738329e006d7f3bf49
saurav kushwaha <147703919+Kingsaurav@users.noreply.github.com>
2024-07-14 16:05:28 +0000

    change the terminal size

---
+++ javaScript_lecture1/Datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // to treat all js code as new version
+//alert(3+3)// we are using node js ,not Browser
+console.log(3
+    +3);//works but necessary to maintain code readability
+console.log("Hello"); // semicolon is optional but it is good pratice to use
+let name="saurav"//string
+let age=21//number
+let isLoggedIn=true;//boolean
+
+
+// number=> 12
+// string=>"" or '' 
+// bigInt
+// boolean=> true/false
+// null=> standalone value or it represent empty value
+// undefined
+//symbol=>unique
+
+
+
+// object => non primitive and having property and behaviour
+
+
+console.log(typeof 12);//number
+// typeof is used to know the type of values or variables
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
