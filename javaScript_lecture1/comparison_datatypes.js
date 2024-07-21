657bc8df9df48bfb590797403dc8e215f6b69bc2
saurav kushwaha <147703919+Kingsaurav@users.noreply.github.com>
2024-07-21 08:45:19 +0000

    comparison of datatypes

---
+++ javaScript_lecture1/comparison_datatypes.js
@@ -0,0 +1,25 @@
+// console.log(2> 1);
+// console.log(2>= 1);
+// console.log(2== 1);
+// console.log(2!= 1);
+// console.log(2< 1);
+
+// console.log("2" > 1);//true  // convert "2" as 2 expicitly
+// console.log("02" > 1);// true
+
+
+// console.log(null > 0);// false
+// console.log(null == 0);// false
+// console.log(null >= 0);// true
+
+
+// console.log(undefined > 0);// false 
+// console.log(undefined == 0);// false
+// console.log(undefined >= 0);// false
+
+// == equality check and comparison work different ways
+
+//  strict check === it checks value as well as data types
+ console.log("2" === 1);// false
+ console.log(2===1);// false
+ console.log(1===1);// true
